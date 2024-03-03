7175f8b1fbddfaaf289fed8615db522d43e56236
kacper-malinowski <263518@student.pwr.edu.pl>
2024-03-03 11:35:04 +0100

    LAB0 dodany

---
+++ LAB0/LAB1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LAB1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+    {
+        FizzBuzz fz1 = new FizzBuzz(16);
+        fz1.Display();
+    }
+}
+}
